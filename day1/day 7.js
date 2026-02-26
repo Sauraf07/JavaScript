af2038a0a75d948e2e051912075009019021f068
Saurav Kumar <your_github_email@gmail.com>
2026-02-26 19:02:26 +0530

    update

--- day1/day 7.js
+++ day1/day 7.js
@@ -1,6 +1,17 @@
-let student = {
-  name: "Saurav",
-  age: 20,
-  course: "JavaScript"
-};
-console.log(student)
+// let student = {
+//   name: "Saurav",
+//   age: 20,
+//   course: "JavaScript"
+// };
+// console.log(student)
+
+let a = 0.1
+let b = 0.2
+let c = a+b
+if (c == 0.3){
+  console.log(true)
+}
+else{
+  console.log(false)
+}
+console.log(c)
