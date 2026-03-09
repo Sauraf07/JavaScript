205b0d825e265ffb719e69015cddf9cee6f1f2fd
Sauraf Kumar <saurafsps7631@gmail.com>
2026-03-09 15:58:15 +0530

    update

---
+++ day1/p1.js
@@ -0,0 +1,3 @@
+let age = 23;
+let result = age >= 18 ? "adult":"minor";
+console.log(result)
---
+++ loops/Number Logic Questions.js
@@ -0,0 +1,34 @@
+// let num = 1234;
+// let rev = 0;
+// while(num>0){
+//     let digit = num %10;
+//     rev = rev *10 +digit;
+
+//     num = Math.floor(num/10);
+// }
+// console.log(rev)
+// --------------------------------------
+// let num = 120;
+// let original = num;
+// let rev =0;
+// while(num>0){
+//     let digit = num %10;
+//     rev = rev *10 +digit
+
+//     num = Math.floor(num/10);
+// }
+// if(original===rev){
+//     console.log("Palindrome");
+// }
+// else(
+//     console.log("Non Palindrome")
+// )
+
+// ---------------------------------------------
+// let num = 5
+// let fact = 1
+// for ( i = 1; i<= num; i++){
+//     fact = fact *i
+// }
+// console.log(fact)
+// --------------------------------------------
